95423a46a865e498e4774a79da80ba64f02eaec2
Chihun Lee <Chihun@live.ie>
2020-03-11 22:20:35 +0000

    Added backend + uploading image api

--- backend.py
+++ backend.py
@@ -2,11 +2,33 @@ from flask import Flask, request, jsonify, Response
 import csv
 import json
 import sqlite3
+import cv2
 import datetime
 import random
 import circle_eval as ce
 
 app = Flask(__name__)
 
+
+@app.route("/api/uploadImage", methods=['POST'])
+def uploadImage():
+    data = request.files
+
+
+    f = open("image.jpg", "w")
+    f.truncate()
+    f.close()
+    with open('image.jpg', 'wb') as f:
+        f.write(data["myFile"].read())
+
+    
+    return "done"
+
+
+
+
 if __name__ == '__main__':
+
+    a = ce.parseArguments([])
+    ce.main(a)
     app.run(host= '0.0.0.0', port="8000")
